669003f1efc0de07fd4e25d9be34d550dd92d719
dhrupo <dhrupo@gmail.com>
2021-02-27 11:10:09 +0600

    first commit

---
+++ src/components/cart/Cart.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Cart = (props) => {
+  const cart = props.cart;
+  const total = cart.reduce((total, product) => total + product.price, 0);
+  let shipping = 0;
+  if (total > 35) {
+    shipping = 0;
+  }
+  else if (total > 15) {
+    shipping = 4.99;
+  }
+  else if (total > 0) {
+    shipping = 12.99;
+  }
+
+  const tax = (total / 10).toFixed(2);
+  const grandTotal = (total + shipping + Number(tax)).toFixed(2);
+
+  return (
+    <div>
+      <h4>Order Summary</h4>
+      <p>Items Ordered: {cart.length}</p>
+      <p>Product Price: {total}</p>
+      <p><small>shipping cost: {shipping}</small></p>
+      <p><small>Tax + VAT: {tax}</small></p>
+      <p>Total Price: {grandTotal}</p>
+    </div>
+  );
+};
+
+export default Cart;
